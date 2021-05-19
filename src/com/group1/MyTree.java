e49b651cc0af0d90b0deaf46c1172b6df8b830a8
MrBlack117 <mrblack1826@gmail.com>
2021-05-19 10:30:38 +0300

    Update MyTree.java

--- src/com/group1/MyTree.java
+++ src/com/group1/MyTree.java
@@ -1,10 +1,26 @@
 package com.group1;
 
 interface MyTree<T extends Comparable<T>> {
+
+  /**
+   * Добавить элемент в дерево
+   *
+   * @param value
+   */
     void insert(T value);
 
+    /**
+     * Удалить элемент из дерева
+     *
+     * @param value
+     */
     void delete(T value);
 
+    /**
+     * Возвращает true, если элемент содержится в дереве
+     *
+     * @param value
+     */
     boolean contains(T value);
 
 }
