7c5b4f6f71888613ec4ac63cc91d506c617c9f8c
MrBlack117 <mrblack1826@gmail.com>
2021-05-19 17:10:07 +0300

    Update Main.java

--- src/com/group1/Main.java
+++ src/com/group1/Main.java
@@ -31,20 +31,20 @@ public class Main {
      SimpleBinarySearchTree<Integer> simpleTree = new SimpleBinarySearchTree<>();
 //
      for (int i = 0; i < 100_000; i++) {
-         SimpleTree.insert(random.nextInt(1_000_000));
+         simpleTree.add(random.nextInt(1_000_000));
      }
      long start = System.nanoTime();
-     simpleTree.delete(random.nextInt(1_000_000));
+     simpleTree.remove(random.nextInt(1_000_000));
      System.out.println("Simple delete took ms: " + (System.nanoTime() - start));
 //
 //
      RedBlackTree<Integer> betterTree = new RedBlackTree<>();
 //
      for (int i = 0; i < 100_000; i++) {
-         betterTree.insert(random.nextInt(1_000_000));
+         betterTree.add(random.nextInt(1_000_000));
      }
      start = System.nanoTime();
-     betterTree.deleteNode(random.nextInt(1_000_000));
+     betterTree.remove(random.nextInt(1_000_000));
      System.out.println("Better delete took ms: " + (System.nanoTime() - start));
  }
 //
@@ -52,7 +52,7 @@ public class Main {
      SimpleBinarySearchTree<Integer> simpleTree = new SimpleBinarySearchTree<>();
 //
      for (int i = 0; i < 100_000; i++) {
-         simpleTree.insert(random.nextInt(1_000_000));
+         simpleTree.add(random.nextInt(1_000_000));
      }
      long start = System.nanoTime();
      boolean contains = simpleTree.contains(5);
@@ -62,10 +62,10 @@ public class Main {
      RedBlackTree <Integer> betterTree = new RedBlackTree<>();
 
      for (int i = 0; i < 100_000; i++) {
-         betterTree.insert(random.nextInt(1_000_000));
+         betterTree.add(random.nextInt(1_000_000));
      }
      start = System.nanoTime();
-     contains = betterTree.searchTree(random.nextInt(1_000_000)) != null;
+     contains = betterTree.contains(5);
      System.out.println("Better contains took ms: " + (System.nanoTime() - start));
  }
 
@@ -74,7 +74,7 @@ public class Main {
      SimpleBinarySearchTree<Integer> simpleTree = new SimpleBinarySearchTree<>();
 
      for (int i = 0; i < 100_000; i++) {
-         simpleTree.insert(random.nextInt(1_000_000));
+         simpleTree.add(random.nextInt(1_000_000));
      }
      System.out.println("Simple insertion took ms: " + (System.nanoTime() - start));
 
@@ -83,7 +83,7 @@ public class Main {
      RedBlackTree<Integer> betterTree = new RedBlackTree<>();
 
      for (int i = 0; i < 100_000; i++) {
-         betterTree.insert(random.nextInt(1_000_000));
+         betterTree.add(random.nextInt(1_000_000));
      }
      System.out.println("Better insertion took ms: " + (System.nanoTime() - start));
  }
