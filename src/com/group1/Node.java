89013f4f34e89b7801294750a606160cbcf9873f
Zu9zwan9 <46656676+Zu9zwan9@users.noreply.github.com>
2021-05-19 16:20:59 +0300

    added comments

--- src/com/group1/Node.java
+++ src/com/group1/Node.java
@@ -2,20 +2,20 @@ package com.group1;
 
 class Node<T extends Comparable<T>> {
     private T value;
-    private Node<T> parent;
-    private Node<T> leftNode;
-    private Node<T> rightNode;
+    private Node<T> parent; // Родитель
+    private Node<T> leftNode; // Левый потомок
+    private Node<T> rightNode; //Правый потомок
 
     public Node(T value, Node<T> leftNode, Node<T> rightNode, Node<T> parent) {
-        this.value = value;
-        this.leftNode = leftNode;
-        this.rightNode = rightNode;
-        this.parent = parent;
+        this.value = value; // Выбранное Значение
+        this.leftNode = leftNode; //Выбранный левый потомок
+        this.rightNode = rightNode; // Выбранный правый потомок
+        this.parent = parent;// Выбранный родитель
     }
 
     public T getValue() {
         return value;
-    }
+    } // Получить значение этого элемента
 
     public Node<T> setValue(T value) {
         this.value = value;
@@ -26,25 +26,25 @@ class Node<T extends Comparable<T>> {
         return leftNode;
     }
 
-    public Node<T> setLeftNode(Node<T> leftNode) { //Установка ссылки на левого потомка
+    public Node<T> setLeftNode(Node<T> leftNode) {
         this.leftNode = leftNode;
         return this;
     }
 
-    public Node<T> getRightNode() { //Получение ссылки на правого потомка
+    public Node<T> getRightNode() {
         return rightNode;
     }
 
-    public Node<T> setRightNode(Node<T> rightNode) { //Установка ссылки на правого потомка
+    public Node<T> setRightNode(Node<T> rightNode) {
         this.rightNode = rightNode;
         return this;
     }
 
-    public Node<T> getParent() { //Получение ссылки на предка
+    public Node<T> getParent() {
         return parent;
     }
 
-    public Node<T> setParent(Node<T> parent) { // Установка ссылки на предка
+    public Node<T> setParent(Node<T> parent) {
         this.parent = parent;
         return this;
     }
