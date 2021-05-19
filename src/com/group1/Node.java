4b2fdc95129ecd71050683c1adadea3e435bc7cb
MrBlack117 <mrblack1826@gmail.com>
2021-05-19 16:16:35 +0300

    Update Node.java

--- src/com/group1/Node.java
+++ src/com/group1/Node.java
@@ -26,25 +26,25 @@ class Node<T extends Comparable<T>> {
         return leftNode;
     }
 
-    public Node<T> setLeftNode(Node<T> leftNode) {
+    public Node<T> setLeftNode(Node<T> leftNode) { //Установка ссылки на левого потомка
         this.leftNode = leftNode;
         return this;
     }
 
-    public Node<T> getRightNode() {
+    public Node<T> getRightNode() { //Получение ссылки на правого потомка
         return rightNode;
     }
 
-    public Node<T> setRightNode(Node<T> rightNode) {
+    public Node<T> setRightNode(Node<T> rightNode) { //Установка ссылки на правого потомка
         this.rightNode = rightNode;
         return this;
     }
 
-    public Node<T> getParent() {
+    public Node<T> getParent() { //Получение ссылки на предка
         return parent;
     }
 
-    public Node<T> setParent(Node<T> parent) {
+    public Node<T> setParent(Node<T> parent) { // Установка ссылки на предка
         this.parent = parent;
         return this;
     }
